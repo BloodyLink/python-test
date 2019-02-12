e4ce0fb554b27032764196b459c516ce3618e929
BloodyLink <david.quezada.m@gmail.com>
2019-02-12 13:43:39 -0300

    menu_admin first approach

---
+++ apps/menu_admin/menu_service.py
@@ -0,0 +1,23 @@
+from datetime import date
+
+from .models import Menu
+
+
+class MenuService():
+
+    @staticmethod
+    def saveMenu(description, date):
+        response = None
+        try:
+            menu = Menu(
+                description = description,
+                date = date,
+            )
+            menu.save()
+            response = 'Menu saved!'
+        except Exception as e:
+            response = 'There was a problem saving the menu: {error}'.format(error=e)
+
+    @staticmethod
+    def getMenuByDate(date):
+        return Menu.object.get(date=date)
--- apps/menu_admin/views.py
+++ apps/menu_admin/views.py
@@ -1,4 +1,20 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# class MenuManager():
-#     pass
+from datetime import date
+
+from .menu_service import MenuService
+
+
+class MenuManager():
+    
+    def __init__(self):
+        self.menuService = MenuService()
+
+    def saveMenu(self, description, date):
+        return self.menuService.saveMenu(
+            description=description,
+            date=date,
+        )
+
+    def getMenuByDate():
+        self.menuService.getMenuByDate(date.today())
